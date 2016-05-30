020cd6db5faa03ed52ee3a771d924d2f329b2d6f
fengqiyue <mynameiszhangxu@gmail.com>
2016-05-30 15:56:00 +0800

    .

--- models/weixin.js
+++ models/weixin.js
@@ -6,12 +6,15 @@ function Weixin (weixin) {
   this.imgurl = weixin.imgurl;  //头像链接
 };
 
+module.exports = Weixins;
+
 //存储节目信息
 Weixin.prototype.save = function(callback) {
   //要存入数据库的节目文档
   var weixin = {
       name: this.name,
-      imgurl:this.imgurl
+      imgurl:this.imgurl,
+      time: new Date().getTime()
   };
    //打开数据库
   mongodb.open(function (err, db) {
@@ -33,6 +36,36 @@ Weixin.prototype.save = function(callback) {
           return callback(err);//错误，返回 err 信息
         }
         callback(null);//成功！err 为 null，并返回存储后的用户文档
+      });
+    });
+  });
+};
+
+Weixin.get = function(name, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);//错误，返回 err 信息
+    }
+    //读取 weixins 集合
+    db.collection('weixins', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);//错误，返回 err 信息
+      }
+      var query = {};
+      if (name) {
+        query.name = name;
+      }
+      //根据 query 对象产询文章
+      collection.find(query).sort(function(a, b) {
+        return a.time - b.time
+      }).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);//失败！返回 err 信息
+        }
+        callback(null, docs);//成功！返回查询的节目信息
       });
     });
   });
